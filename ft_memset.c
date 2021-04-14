60a2cd007b7b2274b6eedeb6f0f5754a1914deda
mcorteslozano <corteslozanomario@gmail.com>
2021-04-14 12:09:52 +0200

    14/04/2021

--- ft_memset.c
+++ ft_memset.c
@@ -3,16 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mcortes- <mcortes-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mario <mario@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/06 16:50:15 by mcortes-          #+#    #+#             */
-/*   Updated: 2021/04/07 19:30:59 by mcortes-         ###   ########.fr       */
+/*   Updated: 2021/04/14 12:09:36 by mario            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
+/*
+*	Name: ft_memset
+*	Parameters: void *b, int c, size_t len
+*	Authorized functions:
+*	Description: 
+*/
 void	*ft_memset(void *b, int c, size_t len)
 {
 	while (len > 0)
--- ft_split.c
+++ ft_split.c
@@ -3,33 +3,11 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mcortes- <mcortes-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mario <mario@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/06 16:55:16 by mcortes-          #+#    #+#             */
-/*   Updated: 2021/04/13 19:58:41 by mcortes-         ###   ########.fr       */
+/*   Updated: 2021/04/14 12:03:56 by mario            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-char	**ft_split(char const *s, char c);
-{
-	int		i;
-	int		len;
-	char	*str;
-
-	i = 0;
-	len = ft_strlen(s);
-	if (!s)
-		return (NULL);
-	str = malloc(sizeof(char) * len);
-	if (!str)
-		return (NULL)
-	while (s[i] != '\0' || s[i] == c)
-	{
-		i++;
-	}
-	if (s[i] == c)
-		
-	
-}
